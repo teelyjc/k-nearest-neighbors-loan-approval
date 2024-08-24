807248bc0d4e5ed34ea6dc287f0f0240d5dc7158
Tannatee Juchan <ju.tannatee2543@gmail.com>
2024-08-25 00:29:32 +0700

    feat: add landing page for app and show all rows and page transition

---
+++ src/app/actions.ts
@@ -0,0 +1,54 @@
+import { LoanApproval, LoanStatus } from "@/domains/LoanApprovement";
+import { getDataStack, getFromDataset } from "@/libs/serializer";
+
+export async function getLoanLength() {
+  const approvements = new Array<LoanApproval>();
+
+  let approved = 0;
+  let rejected = 0;
+
+  const out = getFromDataset("public/loan-approval-dataset.csv");
+  const dataStack = getDataStack(out);
+
+  dataStack.forEach((row) => {
+    approvements.push(
+      new LoanApproval(
+        row[0],
+        parseFloat(row[1]),
+        LoanApproval.parseEducation(row[2]),
+        LoanApproval.parseYesNoToBoolean(row[3]),
+        parseFloat(row[4]),
+        parseFloat(row[5]),
+        parseFloat(row[6]),
+        parseFloat(row[7]),
+        parseFloat(row[8]),
+        parseFloat(row[9]),
+        parseFloat(row[10]),
+        parseFloat(row[11]),
+        LoanApproval.parseApprovement(row[12])
+      )
+    );
+  });
+
+  approvements.forEach((approvement) => {
+    switch (approvement.getLoanStatus()) {
+      case LoanStatus.Approved: {
+        approved++;
+        break;
+      }
+      case LoanStatus.Rejected: {
+        rejected++;
+        break;
+      }
+      default: {
+        throw new Error("Dataset doesn't match to the specified");
+      }
+    }
+  });
+
+  return {
+    all: approvements.length,
+    approved,
+    rejected,
+  };
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,99 @@
+import { NextPage } from "next";
 import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
+import { Fragment } from "react";
 
-export default function Home() {
+import { getLoanLength } from "@/app/actions";
+import { Button } from "@/components/common/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const HomePage: NextPage = async () => {
+  const loneLength = await getLoanLength();
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+    <Fragment>
+      <div className="flex flex-col my-5 max-w-lg mx-auto text-center space-y-5">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Loan Approvement 💵
+        </h1>
+        <p className="leading-7 [&:not(:first-child)]:mt-4">
+          A prediction your <b>loan approval</b> with <b>K-Nearest Neighbors</b>
+          .
+          <br />
+          <br />
+          Regarding your result, accuracy is dependent on the data.
+          <br />
+          Your result accuracy may{" "}
+          <b>
+            <span className="text-red-500">not be 100 percent</span>
+          </b>
+          .
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/images/bg.png"
+          width={600}
+          height={367}
+          alt="background"
+          className="max-w-xs mx-auto"
         />
+        <div className="flex mx-auto space-x-5">
+          <Button variant={"default"} className="w-full" asChild>
+            <Link href="/inquiry">Starting your Prediction</Link>
+          </Button>
+          <Button variant={"secondary"} className="w-full" asChild>
+            <Link href="/explore">Explore</Link>
+          </Button>
+        </div>
+
+        <div className="flex flex-row mx-auto space-x-4">
+          <Card className="hover:scale-110 duration-150 ease-in-out">
+            <CardHeader>
+              <CardTitle>Approved</CardTitle>
+              <CardDescription className="text-sm text-gray-500">
+                Quantity of approved
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p className="text-green-500 font-semibold text-2xl">
+                {Intl.NumberFormat().format(loneLength.approved)}
+              </p>
+            </CardContent>
+          </Card>
+          <Card className="hover:scale-110 duration-150 ease-in-out">
+            <CardHeader>
+              <CardTitle>Rejected</CardTitle>
+              <CardDescription className="text-sm text-gray-500">
+                Quantity of rejected
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p className="text-red-500 font-semibold text-2xl">
+                {Intl.NumberFormat().format(loneLength.rejected)}
+              </p>
+            </CardContent>
+          </Card>
+        </div>
+        <Card className="hover:scale-110 duration-150 ease-in-out">
+          <CardHeader>
+            <CardTitle>Results</CardTitle>
+            <CardDescription className="text-sm text-gray-500">
+              Quantity of all included approved, rejected
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p className="font-semibold text-2xl">
+              {Intl.NumberFormat().format(loneLength.all)}
+            </p>
+          </CardContent>
+        </Card>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </Fragment>
   );
-}
+};
+
+export default HomePage;
