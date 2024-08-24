0bd5b8054adb47b70eb8887b173af172f5019ff9
Tannatee Juchan <ju.tannatee2543@gmail.com>
2024-08-25 01:26:40 +0700

    Revert "fix: try to fix for vercel deployment"
    This reverts commit ac8b3507c07fbc8fe514bd0af3e521c2eb7a3e43.

--- src/libs/serializer.ts
+++ src/libs/serializer.ts
@@ -1,15 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-export async function getFromDataset(pathname: string) {
-  // fixed vercel host
-  if (pathname.startsWith("/")) {
-    const out = await fetch(pathname);
-    const text = await out.text();
-
-    return text.split(/\r?\n/);
-  }
-
+export function getFromDataset(pathname: string) {
   const out = fs.readFileSync(path.join(process.cwd(), pathname), "utf-8");
   return out.split(/\r?\n/);
 }
